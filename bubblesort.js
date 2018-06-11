83a9fca6c22abf3f955e71f5215d9728a40c4eb6
mnoesen <mnoesen@users.noreply.github.com>
2018-06-11 13:43:36 -0500

    initial commit merge sort

---
+++ bubblesort.js
@@ -0,0 +1,6 @@
+const bubbleSort = function(array) {
+  const swap = function(index1, index2){
+    
+  }
+  
+}
---
+++ bubblesort.spec.js
@@ -0,0 +1,7 @@
+describe('Bubble Sort', function(){
+  it('handles an empty array', function(){
+    expect( bubbleSort([]) ).toEqual( [] );
+  });
+});
+
+it('inverts the values in two adjacent indicies')
